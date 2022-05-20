1fa3f33b8fd958b6bb56e0100a96e341e045d31e
augustolfp <augustolfp@gmail.com>
2022-05-20 00:00:29 -0300

    Página de sessões recebe objeto com horários disponíveis da API

--- src/components/MovieSessions/MovieSessions.js
+++ src/components/MovieSessions/MovieSessions.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import React from "react";
+import axios from "axios";
 
 export default function MovieSessions() {
-    const params = useParams();
-    console.log(params)
+    const [sessions, setSessions] = React.useState({});
+    const {idFilme} = useParams();
+    React.useEffect(() => {
+        const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes`);
+
+        promise.then(response => setSessions(response.data));
+        promise.catch(error => console.log("Erro" + error.response.status));
+    },[]);
+    
     return(
         <div>
+            {sessions.title}     
+            {sessions.overview}
         </div>
     );
 }
